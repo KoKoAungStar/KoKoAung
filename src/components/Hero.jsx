15170a25a9a2d5f36de82e51addcceb177170748
Ko Ko Aung <86854260+KoKoAungStar@users.noreply.github.com>
2023-07-24 19:43:33 +0630

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <div>
             <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className='text-[#915eff]'>Ko Ko Aung</span></h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop applications
+              I develop value added web application
             </p>
           </div>
       </div>
