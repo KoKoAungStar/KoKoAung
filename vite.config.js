26e492084082b8e4e59a1a5eab4fd3cfdb29b364
Ko Ko Aung <86854260+KoKoAungStar@users.noreply.github.com>
2024-06-23 23:24:16 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/KoKoAungStar/',
+  base: '/https://kokoaungstar.github.io/KoKoAung/',  // Adjust according to your GitHub Pages URL
   plugins: [react()],
 });
